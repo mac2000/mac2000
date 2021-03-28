1e7b068b549244a7ad2d7add01ca11359e0656ee
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-03-28 13:03:08 +0300

    wip

--- .github/actions/readme/index.js
+++ .github/actions/readme/index.js
@@ -13,7 +13,7 @@ try {
   console.log(`The event payload: ${payload}`);
 
   const content = fs.readFileSync('README.md', 'utf-8');
-  fs.writeFileSync('README.md', content + '\n\nHELLO FROM CUSTOM ACTION\n\n', 'utf-8');
+  fs.writeFileSync('README.md', content + '\n\nHELLO FROM CUSTOM ACTION\n\n');
 } catch (error) {
   core.setFailed(error.message);
 }
