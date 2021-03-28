4bc329fb7a0b4f4d4978e05aa2849db8aec6f7d9
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-03-28 13:34:55 +0300

    escape

--- .github/actions/readme/index.js
+++ .github/actions/readme/index.js
@@ -6,7 +6,7 @@ try {
   const content = fs.readFileSync('README.md', 'utf-8');
   const modified = content + `\n\nHELLO FROM CUSTOM ACTION with ' and " quotes\n\n`;
   console.log('README', modified);
-  core.setOutput("readme", modified);
+  core.setOutput("readme", modified); // .replace(/'/g, `\'`)
 } catch (error) {
   core.setFailed(error.message);
 }
