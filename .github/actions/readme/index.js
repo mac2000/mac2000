43c21171af90d7f93f7e0f582c03f4ab5d7c5185
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-03-28 13:07:36 +0300

    wip

--- .github/actions/readme/index.js
+++ .github/actions/readme/index.js
@@ -4,15 +4,16 @@ const fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  //core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
 
   const content = fs.readFileSync('README.md', 'utf-8');
+  console.log('README', readme);
   fs.writeFileSync('README.md', content + '\n\nHELLO FROM CUSTOM ACTION\n\n');
 } catch (error) {
   core.setFailed(error.message);
