f98a94d41ef25cc7b4782339e9ca1ad51f842148
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-03-28 13:41:34 +0300

    cleanup, wont be used

--- .github/actions/readme/index.js
+++ .github/actions/readme/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 try {
   const content = fs.readFileSync('README.md', 'utf-8');
-  const modified = content + `\n\nHELLO FROM CUSTOM ACTION with ' and " quotes and $ and @ and \${{ github.actions }}\n\n`;
+  const modified = content + `\n\nHELLO FROM CUSTOM ACTION\n\n`;
   console.log('README', modified);
   core.setOutput("readme", modified);
 } catch (error) {
